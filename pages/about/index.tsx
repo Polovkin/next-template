78a0504db253beb6bac17c6c67249693eb6fe60c
polovkin <zimboroda@gmail.com>
2021-02-20 20:19:12 +0200

    ci(json): remove json-server

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,19 +1,11 @@
 import Router from 'next/router'
 import {MainLayout} from "../../layouts/MainLayout";
 
-export default function About({title}) {
+export default function About() {
     return (
             <MainLayout title={'About page'}>
-                <h1>{title}</h1>
+                <h1>About</h1>
             </MainLayout>
 
     );
-}
-About.getInitialProps = async () => {
-    const response = await fetch('${process.env.API_URL}/about')
-    const data = await response.json()
-
-    return {
-        title: data.title
-    }
 }
